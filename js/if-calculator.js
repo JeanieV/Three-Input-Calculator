218fd89c3d97bd80e7dc390e876bc96853a53166
JeanieV <venterjeaniebs@gmail.com>
2023-01-25 19:25:29 +0200

    Basic Challenge
    All the basics were added. Need to add functionality

---
+++ js/if-calculator.js
@@ -0,0 +1,41 @@
+//First User Input and if it is invalid
+let input1 = prompt("Kindly enter your first number: ");
+input1 = parseInt(input1);
+
+
+if (isNaN(input1)) {
+    alert("Kindly enter a numeric value");
+}
+
+
+
+//Second User Input and if it is invalid
+let input2 = prompt("Kindly enter your second number: ");
+input2 = parseInt(input2);
+
+
+if (isNaN(input2)) {
+    alert("Kindly enter a numeric value");
+}
+
+//Third User Input
+let input3 = prompt("Kindly enter a operator ( + , - , * or / ): ");
+
+
+//Conditional
+if (input3 == '+') {
+    document.getElementById("value").innerHTML = input1 + input2;
+}
+else if (input3 == '-') {
+    document.getElementById("value").innerHTML = input1 - input2;
+}
+else if (input3 == '*') {
+    document.getElementById("value").innerHTML = input1 * input2;
+}
+else {
+    document.getElementById("value").innerHTML = input1 / input2;
+}
+
+
+
+
---
+++ js/switch-calculator.js
@@ -0,0 +1,8 @@
+//User Input
+let input1 = prompt("Kindly enter your first number: ");
+input1 = parseInt(input1);
+
+let input2 = prompt("Kindly enter your second number: ");
+input2 = parseInt(input2);
+
+let input3 = prompt("Kindly enter a arithmetic operator: ");
