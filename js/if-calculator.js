e7f9eab31610d2e26de9889c5982a629307f3ef4
JeanieV <venterjeaniebs@gmail.com>
2023-01-26 18:00:57 +0200

    Correct validation
    Added code to correct a users wrong input.

--- js/if-calculator.js
+++ js/if-calculator.js
@@ -1,44 +1,98 @@
-//First User Input and if it is invalid
-let input1 = prompt("Kindly enter your first number: ");
-input1 = parseInt(input1);
+//While loops to re-enter the correct input
+let firstCheck = false;
+let secondCheck = false;
+let thirdCheck = false;
 
-if (isNaN(input1)) {
-    alert("Kindly enter a numeric value");
+let input1;
+let input2;
+let input3;
+
+while (firstCheck == false) {
+
+    //First User Input and if it is invalid
+    input1 = prompt("Kindly enter your first number: ");
+
+
+    if (isNaN(input1)) {
+        alert("Kindly enter a numeric value");
+    }
+    else {
+        input1 = parseInt(input1);
+        firstCheck = true;
+    }
 }
 
+while (secondCheck == false) {
+    //Second User Input and if it is invalid
+    input2 = prompt("Kindly enter your second number: ");
 
 
-//Second User Input and if it is invalid
-let input2 = prompt("Kindly enter your second number: ");
-input2 = parseInt(input2);
 
-
-if (isNaN(input2)) {
-    alert("Kindly enter a numeric value");
+    if (isNaN(input2)) {
+        alert("Kindly enter a numeric value");
+    }
+    else {
+        input2 = parseInt(input2);
+        secondCheck = true;
+    }
 }
 
-//Third User Input
-let input3 = prompt("Kindly enter a operator ( + , - , * or / ): ");
+while (thirdCheck == false) {
+        //Third User Input
+        input3 = prompt("Kindly enter a operator ( + , - , * or / ): ");
 
 
-//Conditional
+        if (input3 == '+' || input3 == '-' || input3 == '*' || input3 == '/') {
+            //Conditional
+            if (input3 == '+') {
+                document.getElementById("value").innerHTML = input1 + input2;
+            }
+            else if (input3 == '-') {
+                document.getElementById("value").innerHTML = input1 - input2;
+            }
+            else if (input3 == '*') {
+                document.getElementById("value").innerHTML = input1 * input2;
+            }
+            else if (input3 == '/') {
+                document.getElementById("value").innerHTML = input1 / input2;
+            }
+            thirdCheck = true;
+            console.log(input1);
+            console.log(input2);
+            console.log(input3);
+        }
+        else {
+            alert("Kindly enter a operator ( + , - , * or / ): ")
+        }
+    }
 
-if (input3 == '+') {
-    document.getElementById("value").innerHTML = input1 + input2;
-}
-else if (input3 == '-') {
-    document.getElementById("value").innerHTML = input1 - input2;
-}
-else if (input3 == '*') {
-    document.getElementById("value").innerHTML = input1 * input2;
-}
-else if (input3 == '/') {
-    document.getElementById("value").innerHTML = input1 / input2;
-}
 
-else if (input3 !== '+' || input3 !== '-' || input3 !== '*' || input3 !== '/') {
-    alert("Kindly enter a operator ( + , - , * or / ): ")
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- js/switch-calculator.js
+++ js/switch-calculator.js
@@ -1,53 +1,83 @@
-//First User Input and if it is invalid
-let input1 = prompt("Kindly enter your first number: ");
-input1 = parseInt(input1);
+//While loops to re-enter the correct input
+let firstCheck = false;
+let secondCheck = false;
+let thirdCheck = false;
+
+let input1;
+let input2;
+let input3;
 
 
-if (isNaN(input1)) {
-    alert("Kindly enter a numeric value");
+//First while loop to check if it is true or false
+while (firstCheck == false) {
+
+    //First User Input and if it is invalid
+    input1 = prompt("Kindly enter your first number: ");
+
+
+    if (isNaN(input1)) {
+        alert("Kindly enter a numeric value");
+    }
+    else {
+        input1 = parseInt(input1);
+        firstCheck = true;
+    }
+
 }
 
+//Second while loop to check if it is true or false
+while (secondCheck == false) {
+
+    //Second User Input and if it is invalid
+    input2 = prompt("Kindly enter your second number: ");
 
 
-//Second User Input and if it is invalid
-let input2 = prompt("Kindly enter your second number: ");
-input2 = parseInt(input2);
-
-
-if (isNaN(input2)) {
-    alert("Kindly enter a numeric value");
+    if (isNaN(input2)) {
+        alert("Kindly enter a numeric value");
+    }
+    else {
+        input2 = parseInt(input2);
+        secondCheck = true;
+    }
 }
 
-//Third User Input
-let input3 = prompt("Kindly enter an operator ( + , - , * or / ): ");
+//Third while loop to check if it is true or false
+while (thirdCheck == false) {
+
+    //Third User Input
+    input3 = prompt("Kindly enter an operator ( + , - , * or / ): ");
 
 
-//Conditional
-if(input3 == '+' || input3 == '-' || input3 == '*' || input3 == '/'){
-    
-switch(input3) {
-    case '+':
-         document.getElementById("values").innerHTML = input1 + input2;
-        break;
+    //Conditional
+    if (input3 == '+' || input3 == '-' || input3 == '*' || input3 == '/') {
 
-    case '-':
-        document.getElementById("values").innerHTML = input1 - input2;
-        break;   
+        switch (input3) {
+            case '+':
+                document.getElementById("values").innerHTML = input1 + input2;
+                break;
 
-    case '*':
-        document.getElementById("values").innerHTML = input1 * input2;
-        break;
+            case '-':
+                document.getElementById("values").innerHTML = input1 - input2;
+                break;
 
-    case '/':
-        document.getElementById("values").innerHTML = input1 / input2;
-        break;
+            case '*':
+                document.getElementById("values").innerHTML = input1 * input2;
+                break;
 
-    default:
-        alert("Invalid operator");
-        break;
+            case '/':
+                document.getElementById("values").innerHTML = input1 / input2;
+                break;
+
+            default:
+                alert("Invalid operator");
+                break;
+        }
+        thirdCheck = true;
+        console.log(input1);
+        console.log(input2);
+        console.log(input3);
+    }
+    else {
+        alert("Kindly enter an operator ( + , - , * or / ): ")
+    }
 }
-}
-else{
-    alert("Kindly enter an operator ( + , - , * or / ): ")
-}
-
